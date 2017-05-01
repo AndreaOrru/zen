437c187eaa8cd4be2fd7e58877da726909d178c6
Andrew Kelley <superjoe30@gmail.com>
2017-05-01 13:15:35 -0400

    update for latest zig

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const Builder = @import("std").build.Builder;
+const builtin = @import("builtin");
 
 pub fn build(b: &Builder) {
     const kernel = b.addExecutable("zen", "kernel/kmain.zig");
@@ -7,7 +8,7 @@ pub fn build(b: &Builder) {
     kernel.addAssemblyFile("kernel/isr.s");
     kernel.addAssemblyFile("kernel/vmem.s");
 
-    kernel.setTarget(Arch.i386, Os.freestanding, Environ.gnu);
+    kernel.setTarget(builtin.Arch.i386, builtin.Os.freestanding, builtin.Environ.gnu);
     kernel.setLinkerScriptPath("kernel/linker.ld");
 
     b.default_step.dependOn(&kernel.step);
