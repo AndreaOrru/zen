0e0e900bae5d49fcaa89ec6da0d73763f2719600
Andrea Orru <andreaorru1991@gmail.com>
2017-05-24 11:52:53 +0200

    Document the scheduler.

--- kernel/scheduler.zig
+++ kernel/scheduler.zig
@@ -8,9 +8,13 @@ const Process = @import("process.zig").Process;
 const Thread = @import("thread.zig").Thread;
 const List = @import("std").LinkedList;
 
-pub var current_process: &Process = undefined;
-var ready_queue: List(&Thread) = undefined;
+pub var current_process: &Process = undefined;  // The process that is currently executing.
+var ready_queue: List(&Thread) = undefined;     // Queue of threads ready for execution.
 
+////
+// Schedule to the next thread in the queue.
+// Called at each timer tick.
+//
 fn schedule() {
     if (ready_queue.popFirst()) |next| {
         ready_queue.append(next);
@@ -22,6 +26,12 @@ fn schedule() {
     }
 }
 
+////
+// Set up a context switch to a thread.
+//
+// Arguments:
+//     thread: The thread to switch to.
+//
 fn contextSwitch(thread: &Thread) {
     switchProcess(thread.process);
 
@@ -29,6 +39,12 @@ fn contextSwitch(thread: &Thread) {
     gdt.setKernelStack(usize(isr.context) + @sizeOf(isr.Context));
 }
 
+////
+// Switch to the address space of a process, if necessary.
+//
+// Arguments:
+//     process: The process to switch to.
+//
 pub fn switchProcess(process: &Process) {
     if (current_process != process) {
         x86.writeCR3(process.page_directory);
@@ -36,16 +52,29 @@ pub fn switchProcess(process: &Process) {
     }
 }
 
+////
+// Add a new thread to the scheduling queue.
+// Schedule it immediately.
+//
+// Arguments:
+//     new_thread: The thread to be added.
+//
 pub fn add(new_thread: &Thread) {
     ready_queue.append(%%ready_queue.createNode(new_thread));
     contextSwitch(new_thread);
 }
 
+////
+// Return the thread currently being executed.
+//
 pub fn current() -> ?&Thread {
     const last = ready_queue.last ?? return null;
     return last.data;
 }
 
+////
+// Initialize the scheduler.
+//
 pub fn initialize() {
     tty.step("Initializing the Scheduler");
 
