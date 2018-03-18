f71f0117ddb065669480722784beb25c2d38b875
Andrea Orru <andrea@orru.io>
2018-03-18 14:46:47 -0400

    Update enum in tty

--- kernel/tty.zig
+++ kernel/tty.zig
@@ -8,22 +8,22 @@ const VGA_HEIGHT = 25;
 
 // Color codes.
 pub const Color = enum(u4) {
-    Black,         // 0
-    Blue,          // 1
-    Green,         // 2
-    Cyan,          // 3
-    Red,           // 4
-    Magenta,       // 5
-    Brown,         // 6
-    LightGrey,     // 7
-    DarkGrey,      // 8
-    LightBlue,     // 9
-    LightGreen,    // 10
-    LightCyan,     // 11
-    LightRed,      // 12
-    LightMagenta,  // 13
-    LightBrown,    // 14
-    White,         // 15
+    Black        = 0,
+    Blue         = 1,
+    Green        = 2,
+    Cyan         = 3,
+    Red          = 4,
+    Magenta      = 5,
+    Brown        = 6,
+    LightGrey    = 7,
+    DarkGrey     = 8,
+    LightBlue    = 9,
+    LightGreen   = 10,
+    LightCyan    = 11,
+    LightRed     = 12,
+    LightMagenta = 13,
+    LightBrown   = 14,
+    White        = 15,
 };
 
 // Character with attributes.
