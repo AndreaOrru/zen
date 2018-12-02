d9dec753848fa0b818feab51932717a55ba5aafc
Andrea Orru <andrea@orru.io>
2018-12-02 17:52:22 +0000

    Run Qemu step

--- build.zig
+++ build.zig
@@ -5,6 +5,21 @@ const builtin = @import("builtin");
 pub fn build(b: *Builder) void {
     const loader = buildLoader(b);
     const kernel = buildKernel(b);
+
+    setupQemu(b, loader, kernel);
+}
+
+/// Setup a command to run Zen inside Qemu.
+fn setupQemu(b: *Builder, loader: []const u8, kernel: []const u8) void {
+    const qemu = b.step("qemu", "Run Zen inside Qemu");
+    const run_qemu = b.addCommand(".", b.env_map, [][]const u8 {
+        "qemu-system-x86_64",
+        "-display", "curses",
+        "-kernel", loader,
+        "-initrd", kernel,
+    });
+    run_qemu.step.dependOn(b.default_step);
+    qemu.dependOn(&run_qemu.step);
 }
 
 /// Build the loader (32-bit springboard).
