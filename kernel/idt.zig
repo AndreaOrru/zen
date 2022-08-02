c095f49d3bc231c6b556c8b7404a6ea3bf093130
Lion Kortlepel <development@kortlepel.com>
2022-08-02 10:15:31 +0200

    update idt, isr to zig 0.9.1

--- kernel/idt.zig
+++ kernel/idt.zig
@@ -5,30 +5,30 @@ const x86 = @import("x86.zig");
 
 // Types of gates.
 pub const INTERRUPT_GATE = 0x8E;
-pub const SYSCALL_GATE   = 0xEE;
+pub const SYSCALL_GATE = 0xEE;
 
 // Structure representing an entry in the IDT.
 const IDTEntry = packed struct {
-    offset_low:  u16,
-    selector:    u16,
-    zero:        u8,
-    flags:       u8,
+    offset_low: u16,
+    selector: u16,
+    zero: u8,
+    flags: u8,
     offset_high: u16,
 };
 
 // IDT descriptor register.
 const IDTRegister = packed struct {
     limit: u16,
-    base:  *[256]IDTEntry,
+    base: *[256]IDTEntry,
 };
 
 // Interrupt Descriptor Table.
 var idt: [256]IDTEntry = undefined;
 
 // IDT descriptor register pointing at the IDT.
-const idtr = IDTRegister {
+const idtr = IDTRegister{
     .limit = u16(@sizeOf(@typeOf(idt))),
-    .base  = &idt,
+    .base = &idt,
 };
 
 ////
@@ -39,14 +39,14 @@ const idtr = IDTRegister {
 //     flags: Type and attributes.
 //     offset: Address of the ISR.
 //
-pub fn setGate(n: u8, flags: u8, offset: extern fn()void) void {
+pub fn setGate(n: u8, flags: u8, offset: fn () void) void {
     const intOffset = @ptrToInt(offset);
 
-    idt[n].offset_low  = @truncate(u16, intOffset);
+    idt[n].offset_low = @truncate(u16, intOffset);
     idt[n].offset_high = @truncate(u16, intOffset >> 16);
-    idt[n].flags       = flags;
-    idt[n].zero        = 0;
-    idt[n].selector    = gdt.KERNEL_CODE;
+    idt[n].flags = flags;
+    idt[n].zero = 0;
+    idt[n].selector = gdt.KERNEL_CODE;
 }
 
 ////
