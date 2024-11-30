8c447ff9a4f442ec35724034a4309556ab56cc87
Andrea Orru <andreaorru1991@gmail.com>
2024-11-30 15:24:46 +0900

    Bitmap font

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -1,12 +1,12 @@
 const limine = @import("limine");
 const std = @import("std");
 
-/// The base revision of the Limine protocol that the kernel supports.
+/// Base revision of the Limine protocol that the kernel supports.
 pub export var base_revision: limine.BaseRevision linksection(".limine_requests") = .{
     .revision = 2, // TODO(2): Support base revision 3.
 };
 
-/// Completely stop the CPU.
+/// Completely stops the CPU.
 inline fn hang() noreturn {
     asm volatile ("cli");
     while (true) {
@@ -14,14 +14,14 @@ inline fn hang() noreturn {
     }
 }
 
-/// The kernel's global panic handler.
+/// Kernel's global panic handler.
 pub fn panic(_: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
     // TODO(1): Implement a useful panic handler.
     // TODO(2): Support stack traces.
     hang();
 }
 
-/// The entry point for the kernel.
+/// Entry point for the kernel.
 export fn _start() callconv(.C) noreturn {
     // Don't proceed if the kernel's base revision is not supported by the bootloader.
     if (!base_revision.is_supported()) {
---
+++ kernel/src/tty/font.zig
@@ -0,0 +1,19 @@
+const assert = @import("std").debug.assert;
+
+/// Number of glyphs in the font bitmap.
+pub const NUM_GLYPHS = 128;
+
+/// Width of each font glyph in pixels.
+pub const WIDTH = 8;
+/// Height of each font glyph in pixels.
+pub const HEIGHT = 16;
+
+/// Terminus 8x16 font (ter-i16n) in bitmap format.
+const RAW_BITMAP = @embedFile("font.bin");
+comptime {
+    // Ensure the bitmap has the expected size.
+    assert(RAW_BITMAP.len == NUM_GLYPHS * HEIGHT);
+}
+
+/// Terminus 8x16 font (ter-i16n) as a 2D array of bytes.
+pub const BITMAP: *const [NUM_GLYPHS][HEIGHT]u8 = @ptrCast(RAW_BITMAP);
