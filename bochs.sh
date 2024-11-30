fb52b9ec9e213ebe3555fa923c40d54eeb3a1a94
Andrea Orru <andreaorru1991@gmail.com>
2024-11-30 11:48:45 +0900

    Delete all code

--- bochs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-bochs -q -f run/bochsrc
--- iso.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cp zen                       run/iso/boot/
-cp servers/terminal/terminal run/iso/servers/
-cp servers/keyboard/keyboard run/iso/servers/
-cp servers/shell/shell       run/iso/servers/
-
-grub-mkrescue -o run/zen.iso run/iso/
