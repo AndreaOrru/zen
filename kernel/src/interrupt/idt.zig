8af26804cfa479f8195c1593a1e2843cf628496b
Andrea Orru <andreaorru1991@gmail.com>
2024-12-01 16:29:16 +0900

    Adjust some types in idt

--- kernel/src/interrupt/idt.zig
+++ kernel/src/interrupt/idt.zig
@@ -1,4 +1,5 @@
 const gdt = @import("../cpu/gdt.zig");
+const isr = @import("./isr.zig");
 const term = @import("../term/terminal.zig");
 const x64 = @import("../cpu/x64.zig");
 
@@ -13,7 +14,7 @@ const GateDescriptor = packed struct {
     /// Offset (bits 0 to 15).
     offset_low: u16,
     /// GDT code segment selector.
-    code_segment: u16,
+    code_segment: gdt.SegmentSelector,
     /// Interrupt Stack Table offset.
     ist: u8,
     /// Gate type.
@@ -31,12 +32,14 @@ const GateDescriptor = packed struct {
 };
 
 /// Interrupt Descriptor Table.
-const idt: [NUM_ENTRIES]GateDescriptor linksection(".bss") = undefined;
+var idt: [NUM_ENTRIES]GateDescriptor linksection(".bss") = undefined;
 
 /// Initializes the Interrupt Descriptor Table.
 pub fn initialize() void {
     term.step("Initializing Interrupt Descriptor Table", .{});
 
+    // Connect interrupt gates to Interrupt Service Routines.
+    isr.install();
     // Load the new Interrupt Descriptor Table.
     x64.lidt(.{
         .limit = @sizeOf(@TypeOf(idt)) - 1,
@@ -48,14 +51,14 @@ pub fn initialize() void {
 
 /// Setups an Interrupt Descriptor Table entry.
 /// Parameters:
-///   n:    Index of the gate.
-///   dpl:  Descriptor Privilege Level.
-///   isr:  Address of the Interrupt Service Routine function.
-pub fn setupGate(n: u8, dpl: gdt.Dpl, isr: *const fn () void) void {
+///   n:        Index of the gate.
+///   dpl:      Descriptor Privilege Level.
+///   isr_ptr:  Address of the Interrupt Service Routine function.
+pub fn setupGate(n: u8, dpl: gdt.Dpl, isr_ptr: *const isr.IsrFunction) void {
     // Split the ISR function's offset.
-    const offset = @intFromPtr(isr);
+    const offset = @intFromPtr(isr_ptr);
     const offset_low: u16 = @truncate(offset);
-    const offset_high: u48 = offset >> 16;
+    const offset_high: u48 = @truncate(offset >> 16);
 
     // Setup the corresponding entry in the IDT.
     idt[n] = .{
